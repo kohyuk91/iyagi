c74aa9d4c112a9a9aac3ac0ae1d4ba91506be67d
kohyuk91 <kohyuk91@gmail.com>
2025-11-16 11:02:20 +0900

    Navbar 컨테이너를 전체 너비로 설정

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,7 +18,7 @@ export function Navbar({ className }: NavbarProps) {
         className
       )}
     >
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link
             href="/"
